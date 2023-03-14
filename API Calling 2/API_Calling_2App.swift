ffc430d7ff2b44f0e38a01d3a594e06eb92d85ab
JJKania9711 <116130334+JJKania9711@users.noreply.github.com>
2023-03-14 07:44:30 -0700

    Initial Commit

---
+++ API Calling 2/API_Calling_2App.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling_2App.swift
+//  API Calling 2
+//
+//  Created by James on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
